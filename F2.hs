2f3ef3280ef82f6f330981e2656649399e34bbf6
Max Bergdahl <maxbergdahl@gmail.com>
2015-10-15 15:51:19 +0200

    Almost finished seqDistance, todo: error mesage (using function 'error') if compare dna & prot.

--- F2.hs
+++ F2.hs
@@ -33,3 +33,30 @@ seqLength (DNA (n, (s:b))) = 1 + seqLength (DNA (n, b))
 seqLength (DNA (n, s)) = 0
 seqLength (PROT (n, (s:b))) = 1 + seqLength (PROT (n, b))
 seqLength (PROT (n, s)) = 0
+
+seqDifference :: String -> String -> Int
+seqDifference (s:d) (z:x)
+	| s /= z = 1 + seqDifference d x
+	| otherwise = 0 + seqDifference d x
+seqDifference s z
+	| s /= z = 1
+	| otherwise = 0
+
+difRatio :: MolSeq -> MolSeq -> Double
+difRatio (DNA(n, s)) (DNA(m, z)) = fromIntegral(seqDifference s z) / fromIntegral(seqLength (DNA(n, s)))
+difRatio (PROT(n, s)) (PROT(m, z)) = fromIntegral(seqDifference s z) / fromIntegral(seqLength (PROT(n, s)))
+
+protDifRatio :: MolSeq -> MolSeq -> Double
+protDifRatio (PROT(n, s)) (PROT(m, z)) = fromIntegral(seqDifference s z) / fromIntegral(seqLength (PROT(n, s)))
+
+-- log == natural logarithm
+-- todo error message (using function error) if dna & prot comparison
+seqDistance :: MolSeq -> MolSeq -> Double
+seqDistance (DNA(n, s)) (DNA(m, z))
+	| difRatio (DNA(n, s)) (DNA(m, z)) <= 0.74 = ((-3/4) * log(1-4*((difRatio (DNA(n, s)) (DNA(m, z)))/3)))
+	| otherwise = 3.3
+seqDistance (PROT(n, s)) (PROT(m, z))
+	| difRatio (PROT(n, s)) (PROT(m, z)) <= 0.94 = ((-19/20) * log(1-20*((difRatio (PROT(n, s)) (PROT(m, z)))/19)))
+	| otherwise = 3.7
+
+--seqDistnace PROT (n, s) PROT (m, z) = 
